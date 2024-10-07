41b1628ace054e375deb18bef34c07d5c7518c06
Jocelyn <jocelyn.chan042@gmail.com>
2024-10-06 22:24:04 -0400

    push day 1

--- flowers.js
+++ flowers.js
@@ -4,4 +4,14 @@ onload = () => {
       clearTimeout(c);
     }, 1000);
   };
-  
+
+//welcome message load in 
+document.addEventListener('DOMContentLoaded', (event) => {
+    setTimeout(() => {
+        const welcomeElements = document.querySelectorAll('.welcome');
+        welcomeElements.forEach(element => {
+            element.classList.add('visible');
+        });
+    }, 500);
+});
+
